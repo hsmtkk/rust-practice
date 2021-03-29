21e3c7b2326a33bbeb80fc8b671c6dda995a570f
kouki <kouki@localhost>
2021-03-29 17:12:09 +0900

    implemented file reader

--- samplecli/src/main.rs
+++ samplecli/src/main.rs
@@ -1,28 +1,35 @@
-use clap::{App, Arg};
+use clap::Clap;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+#[derive(Clap, Debug)]
+#[clap(
+    name = "My RPN Program",
+    version = "1.0.0",
+    author = "hsmtkk",
+    about = "Supre awesome sample RPN calculator"
+)]
+
+struct Opts{
+    #[clap(short, long)]
+    verbose: bool,
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
+}
 
 fn main(){
-    let matches = App::new("My RPN program")
-    .version("1.0.0")
-    .author("hsmtkk")
-    .about("Super awesome sample RPN calculator")
-    .arg(Arg::new("formula_file")
-        .about("Formulas written in RPN")
-        .value_name("FILE")
-        .index(1)
-        .required(false)
-    )
-    .arg(Arg::new("verbose")
-        .about("Sets the level of verbosity")
-        .short('v')
-        .long("verbose")
-        .required(false)
-    )
-    .get_matches();
+    let opts = Opts::parse();
 
-    match matches.value_of("formula_file"){
-        Some(file) => println!("File specified: {}", file),
-        None => println!("No file specified."),
+    if let Some(path) = opts.formula_file {
+        let f = File::open(path).unwrap();
+        let reader = BufReader::new(f);
+
+        for line in reader.lines(){
+            let line = line.unwrap();
+            println!("{}", line);
+        }
+    }else{
+        println!("No file is specified");
     }
-    let verbose = matches.is_present("verbose");
-    println!("Is verbosity specified?: {}", verbose);
 }
