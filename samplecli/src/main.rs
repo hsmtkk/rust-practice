a4cc3df969a5b59e4031270c920dc0f490ee6e6b
kouki <kouki@localhost>
2021-03-30 09:52:35 +0900

    implementing rpn calculator

--- samplecli/src/main.rs
+++ samplecli/src/main.rs
@@ -19,23 +19,67 @@ struct Opts{
     formula_file: Option<String>,
 }
 
+struct RpnCalculator(bool);
+
+impl RpnCalculator{
+    pub fn new(verbose:bool) -> Self{
+        Self(verbose)
+    }
+    pub fn eval(&self, formula:&str) -> i32{
+        let mut tokens = formula.split_whitespace().rev().collect::<Vec<_>>();
+        self.eval_inner(&mut tokens);
+        0
+    }
+    fn eval_inner(&self, tokens:&mut Vec<&str>) -> i32{
+        let mut stack = Vec::new();
+
+        while let Some(token) = tokens.pop(){
+            if let Ok(x) = token.parse::<i32>(){
+                stack.push(x);
+            }else{
+                let y = stack.pop().expect("invalid syntax");
+                let x = stack.pop().expect("invalid syntax");
+                let res = match token{
+                    "+" => x + y,
+                    "-" => x - y,
+                    "*" => x * y,
+                    "/" => x / y,
+                    "%" => x % y,
+                    _ => panic!("invalid token"),
+                };
+                stack.push(res);
+            }
+            if self.0 {
+                println!("{:?} {:?}", tokens, stack);
+            }
+        }
+        if stack.len() == 1{
+            stack[0]
+        }else{
+            panic!("invalid syntax");
+        }
+    }
+}
+
 fn main(){
     let opts = Opts::parse();
 
     if let Some(path) = opts.formula_file {
         let f = File::open(path).unwrap();
         let reader = BufReader::new(f);
-        read_file(reader, opts.verbose);
+        read_input(reader, opts.verbose);
     }else{
         let stdin = stdin();
         let reader = stdin.lock();
-        read_file(reader, opts.verbose);
+        read_input(reader, opts.verbose);
     }
 }
 
-fn read_file<R:BufRead>(reader:R, verbose:bool){
+fn read_input<R:BufRead>(reader:R, verbose:bool){
+    let calc = RpnCalculator::new(verbose);
     for line in reader.lines(){
         let line = line.unwrap();
-        println!("{}", line);
+        let answer = calc.eval(&line);
+        println!("{}", answer);
     }
 }
